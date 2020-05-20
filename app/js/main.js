6a1aa99a87e0b130aeeae84e02cadd7a062c70d5
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-05-20 14:47:19 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,19 @@
 $(function(){
 
+    var mixer = mixitup('.products__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 4,
+        starWidth: "12px",
+        ratedFill: "#ffa726",
+        readOnly: true
+    });
 
-
-
-
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
     
 });
