7e22c85af159b091ed21679a17eb8f826b7af092
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-05-22 21:04:28 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,27 @@
 $(function(){
+    $('.icon-th-list').on('click', function(){
+        $('.product-page__items>.products__item').addClass('list'),
+        $('.icon-th-list').addClass('active'),
+        $('.icon-th-large').removeClass('active')
 
-    var mixer = mixitup('.products__inner-box');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product-page__items>.products__item').removeClass('list'),
+        $('.icon-th-large').addClass('active'),
+        $('.icon-th-list').removeClass('active')
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        grid: false
+    });
+
+
 
     $(".rate-star").rateYo({
         rating: 4,
@@ -9,11 +30,21 @@ $(function(){
         readOnly: true
     });
 
+    var mixer = mixitup('.products__inner-box');
+
+    
+
+    
+
     $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
     });
+
+    
+    
+    
     
 });
