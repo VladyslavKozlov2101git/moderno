c6c4828129f0302055ccdb528546fb95daaf62aa
vladyslavkozlov2101git <vladyslavkozlov2101git@gmail.com>
2020-05-26 14:02:03 +0300

    added responsive

--- app/js/main.js
+++ app/js/main.js
@@ -30,9 +30,15 @@ $(function(){
         readOnly: true
     });
 
-    var mixer = mixitup('.products__inner-box');
-
     
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
 
     
 
@@ -42,6 +48,8 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
     });
+
+    var mixer = mixitup('.products__inner-box');
 
     
     
